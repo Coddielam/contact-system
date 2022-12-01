6308525045b03a8699a56b25e24d5f626ee7d557
Eddie Lam <eddie.cm.lam@gtomato.com>
2022-12-01 18:56:16 +0800

    feat: update theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   theme: {
-    colors: {},
-    extend: {},
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
+    },
+    extend: {
+      colors: {
+        appbackground: "#E2E8F0",
+      },
+    },
   },
   plugins: [],
   content: ["./src/**/*.{js,jsx,ts,tsx,html}", "./public/index.html"],
