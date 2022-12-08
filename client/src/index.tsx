6650789a2b311df7f569478b2cb56a4f233a2adb
Eddie Lam <eddie.cm.lam@gtomato.com>
2022-12-08 11:34:48 +0800

    feat: error boundary

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,10 +2,13 @@ import ReactDOM from "react-dom/client";
 import "./styles/tailwind.css";
 import App from "./App";
 import React from "react";
+import { ErrorBoundary } from "./components/ErrorBoundary/ErrorBoundary";
 
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <ErrorBoundary>
+      <App />
+    </ErrorBoundary>
   </React.StrictMode>
 );
